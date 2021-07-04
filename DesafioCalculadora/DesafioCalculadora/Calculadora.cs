cef5f115473d029661fbdede67cee75302f8dea0
Samyra França <samy_franca@outlook.com>
2021-07-04 20:42:45 -0300

    desafio calculadora

---
+++ DesafioCalculadora/DesafioCalculadora/Calculadora.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DesafioCalculadora
+{
+    public class Calculadora
+    {
+        public Calculadora(double valorInvestido)
+        {
+            ValorInvestido = valorInvestido;
+        }
+
+        public double ValorInvestido { get; set; }
+        public double CliquesPorVisualizacao { get; set; }
+        public double QtdCompartilhamentos { get; set; }
+        public double TotalVisualizacoes { get; set; }
+
+        public void CalcularAlcanceTotal()
+        {
+            CalcularVisualizacoesPorValorInvestido();
+
+            CalcularCliquesPorVizualizacao();
+
+            CalcularCompartilhamentos();
+
+            CalcularTotalVizualizacoes();
+        }
+
+        public void CalcularVisualizacoesPorValorInvestido()
+        {
+            TotalVisualizacoes = ValorInvestido > 0 ? (ValorInvestido * 30) : 0;
+        }
+
+        public void CalcularCliquesPorVizualizacao()
+        {
+            CliquesPorVisualizacao = TotalVisualizacoes > 0 ? (TotalVisualizacoes / 100) * 12 : 0;
+        }
+
+        public void CalcularCompartilhamentos()
+        {
+            QtdCompartilhamentos = CliquesPorVisualizacao > 0 ? (CliquesPorVisualizacao / 20) * 3 : 0;
+            QtdCompartilhamentos = QtdCompartilhamentos > 4.0 ? 4.0 : QtdCompartilhamentos;
+        }
+
+        public void CalcularTotalVizualizacoes()
+        {
+            TotalVisualizacoes += QtdCompartilhamentos > 0 ? (QtdCompartilhamentos * 40) : 0;
+        }
+    }
+}
---
+++ DesafioCalculadora/DesafioCalculadoraTest/CalculadoraTest.cs
@@ -0,0 +1,107 @@
+﻿using DesafioCalculadora;
+using NUnit.Framework;
+using System;
+
+namespace DesafioCalculadoraTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void CalcularVisualizacoesPorValorInvestido_ComValorValido()
+        {
+            var calculadora = new Calculadora(1.0);
+            calculadora.CalcularVisualizacoesPorValorInvestido();
+            Assert.AreEqual(30.0, calculadora.TotalVisualizacoes);
+        }
+
+        [Test]
+        public void CalcularVisualizacoesPorValorInvestido_ComValorInvalido()
+        {
+            var calculadora = new Calculadora(-1.0);
+            calculadora.CalcularVisualizacoesPorValorInvestido();
+            Assert.AreEqual(0, calculadora.TotalVisualizacoes);
+        }
+
+        [Test]
+        public void CalcularCliquesPorVisualizacao_ComValorValido()
+        {
+            var calculadora = new Calculadora(4.0);
+            calculadora.TotalVisualizacoes = 120;
+            calculadora.CalcularCliquesPorVizualizacao();
+            Assert.AreEqual(14.4, Convert.ToDecimal(calculadora.CliquesPorVisualizacao));
+        }
+
+        [Test]
+        public void CalcularCliquesPorVisualizacao_ComValorInvalido()
+        {
+            var calculadora = new Calculadora(-4.0);
+            calculadora.TotalVisualizacoes = -120;
+            calculadora.CalcularCliquesPorVizualizacao();
+            Assert.AreEqual(0, Convert.ToDecimal(calculadora.CliquesPorVisualizacao));
+        }
+
+        [Test]
+        public void CalcularCompartilhamentos_ComValorValido()
+        {
+            var calculadora = new Calculadora(1.0);
+            calculadora.CliquesPorVisualizacao = 20;
+            calculadora.CalcularCompartilhamentos();
+            Assert.AreEqual(3.0, Convert.ToDecimal(calculadora.QtdCompartilhamentos));
+        }
+
+        [Test]
+        public void CalcularCompartilhamentos_ComValorInvalido()
+        {
+            var calculadora = new Calculadora(1.0);
+            calculadora.CliquesPorVisualizacao = -20;
+            calculadora.CalcularCompartilhamentos();
+            Assert.AreEqual(0, Convert.ToDecimal(calculadora.QtdCompartilhamentos));
+        }
+
+        [Test]
+        public void CalcularCompartilhamentos_ComValorMaiorQueQuatro()
+        {
+            var calculadora = new Calculadora(1.0);
+            calculadora.CliquesPorVisualizacao = 40;
+            calculadora.CalcularCompartilhamentos();
+            Assert.AreEqual(4.0, Convert.ToDecimal(calculadora.QtdCompartilhamentos));
+        }
+
+        [Test]
+        public void CalcularTotalVisualizacoes_ComValorValido()
+        {
+            var calculadora = new Calculadora(1.0);
+            calculadora.QtdCompartilhamentos = 1;
+            calculadora.CalcularTotalVizualizacoes();
+            Assert.AreEqual(40, Convert.ToDecimal(calculadora.TotalVisualizacoes));
+        }
+
+        [Test]
+        public void CalcularTotalVisualizacoes_ComValorInvalido()
+        {
+            var calculadora = new Calculadora(1.0);
+            calculadora.QtdCompartilhamentos = -1;
+            calculadora.CalcularTotalVizualizacoes();
+            Assert.AreEqual(0, Convert.ToDecimal(calculadora.TotalVisualizacoes));
+        }
+
+        [Test]
+        public void CalcularAlcanceTotal_ComValorValido()
+        {
+            var calculadora = new Calculadora(1.0);
+            calculadora.CalcularAlcanceTotal();
+            Assert.AreEqual(51.6, Convert.ToDecimal(calculadora.TotalVisualizacoes));
+        }
+
+        [Test]
+        public void CalcularAlcanceTotal_ComValorInvalido()
+        {
+            var calculadora = new Calculadora(-1.0);
+            calculadora.CalcularAlcanceTotal();
+            Assert.AreEqual(0, Convert.ToDecimal(calculadora.TotalVisualizacoes));
+        }
+    }
+}
